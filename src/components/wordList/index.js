1544b47a56d6c3157eb5e71ae7827e9b8379791b
squgeim <shreyadahal@gmail.com>
2018-11-10 18:13:37 +0545

    style list

--- src/components/wordList/index.js
+++ src/components/wordList/index.js
@@ -12,6 +12,25 @@ class WordList extends BaseElement {
 
   render() {
     this.html`
+      <style>
+        :host {
+          display: block;
+          width: 100%;
+        }
+        ul li {
+          display: inline-block;
+          line-height: 30px;
+          background: rgba(255, 255, 255, 0.5);
+          float: left;
+          margin: 10px;
+          padding: 10px;
+          cursor: pointer;
+          transition: box-shadow 500ms;
+        }
+        ul li:hover {
+          box-shadow: #ccc 0 0 50px;
+        }
+      </style>
       <ul>
         ${this.wordList}
       </ul>
