a664963372680341d5ec542b7c149f72df1a64f4
squgeim <shreyadahal@gmail.com>
2018-11-11 22:22:59 +0545

    add service worker to support offline app

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 format:
-	prettier --single-quote --trailing-comma es5 --write "src/**/*.js"
+	@prettier --single-quote --trailing-comma es5 --write "**/*.js"
 
 serve:
-	python2 -m SimpleHTTPServer
+	@python2 -m SimpleHTTPServer
+
+list-files:
+	@find src -type f -name "*.js" -o -name "*.css" | jq -R '[inputs | select(length>0)]'
---
+++ sw.js
@@ -0,0 +1,54 @@
+const VERSION = 1;
+const FILES = [
+  'src/utils/misc.js',
+  'src/utils/history.js',
+  'src/utils/model.js',
+  'src/utils/baseElement.js',
+  'src/models/dictionary.js',
+  'src/models/history.js',
+  'src/components/wordList/index.js',
+  'src/components/searchInput/index.js',
+  'src/components/header/index.js',
+  'src/pages/home.js',
+  'src/normalize.css',
+  'src/root.css',
+];
+
+const cacheName = `vocab-${VERSION}`;
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  const matchedCache = (() => {
+    switch (url.hostname) {
+      case 'unpkg.com':
+        return 'unpkg';
+      case 'fonts.google.com':
+        return 'fonts';
+      default:
+        return cacheName;
+    }
+  })();
+
+  event.respondWith(
+    caches.open(matchedCache).then(cache => {
+      return cache.match(event.request).then(response => {
+        return (
+          response ||
+          fetch(event.request).then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+        );
+      });
+    })
+  );
+});
